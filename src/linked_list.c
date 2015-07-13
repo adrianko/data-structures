fe439d8cfc52c9e134be4fc1f5393714ed170692
Adrian Ko <adrianchko@gmail.com>
2015-07-13 13:13:01 +0100

    Add head node

--- src/linked_list.c
+++ src/linked_list.c
@@ -1,7 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main() {
+typedef struct node {
+    int value;
+    struct node *next;
+} Node;
 
+int main() {
+    Node *n = malloc(sizeof(Node));
+    n->value = 5;
+    printf("%d\n", n->value);
     return 0;
 }
