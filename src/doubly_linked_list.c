4dd30d1b6c2b33f19c63dde66f6115746e4cfe31
Adrian Ko <adrianchko@gmail.com>
2015-07-24 09:34:42 +0100

    Create standard list_new

--- src/doubly_linked_list.c
+++ src/doubly_linked_list.c
@@ -4,6 +4,14 @@
 #include "list.h"
 #include "doubly_linked_list.h"
 
+List * list_new() {
+    List *list = malloc(sizeof(List));
+    list->next = NULL;
+    list->size = 0;
+
+    return list;
+}
+
 int main() {
 
     return 0;
